c04ff64e70dee27cedff965c288f9f74c04f319a
seif eddine bissa <seif eddine bissa>
2025-02-11 13:54:17 +0100

    first commit

---
+++ Microservices/GestionCourse/src/main/java/tn/esprit/gestioncourse/GestionCourseApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.gestioncourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCourseApplication.class, args);
+    }
+
+}
---
+++ Microservices/GestionCourse/src/test/java/tn/esprit/gestioncourse/GestionCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.gestioncourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
